e55fcb74cd0210a8c1c676a265adde820fbc3057
hidenvar <s.amirm.fazeli@gmail.com>
2025-01-28 11:24:33 +0330

    fix(bash): stimulate with vcd.

--- src/mul_1x1/mul_1x1_simulate.sh
+++ src/mul_1x1/mul_1x1_simulate.sh
@@ -12,6 +12,6 @@ ghdl -a --workdir=../work mul_1x1.vhdl && \
 ghdl -s --workdir=../work mul_1x1_tb.vhdl && \
 ghdl -a --workdir=../work mul_1x1_tb.vhdl && \
 ghdl -e --workdir=../work mul_1x1_tb && \
-ghdl -r --workdir=../work mul_1x1_tb --wave=mul_1x1.ghw --stop-time=50ns &&\
+ghdl -r --workdir=../work mul_1x1_tb --vcd=mul_1x1.vcd --stop-time=50ns &&\
 
 echo "mul_1x1.ghw is ready"
