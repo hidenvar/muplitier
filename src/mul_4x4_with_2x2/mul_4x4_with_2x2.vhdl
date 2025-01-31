b470f032351faf8d03ac80fa03eafe309d3ff416
Pouya <pouya.t.003@gmail.com>
2025-01-31 03:55:55 +0330

    refactor: change mul_4x4_with_2x2 - shift using custom shift registers - add pp's using custom adders

--- src/mul_4x4_with_2x2/mul_4x4_with_2x2.vhdl
+++ src/mul_4x4_with_2x2/mul_4x4_with_2x2.vhdl
@@ -1,6 +1,5 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
 
 entity mul_4x4_with_2x2 is
     Port (
@@ -19,56 +18,54 @@ architecture structural of mul_4x4_with_2x2 is
         );
     end component;
 
-    -- partial products
-    signal pp1, pp2, pp3, pp4 : std_logic_vector(3 downto 0);
+    component adder_8bit
+        Port (
+            a, b : in std_logic_vector(7 downto 0);
+            sum : out std_logic_vector(7 downto 0);
+            cout : out std_logic
+        );
+    end component;
 
-    -- partial products after shift
-    signal pp1_shift, pp2_shift, pp3_shift, pp4_shift : unsigned(7 downto 0);
+    component shift_left_4
+        Port (
+            input : in std_logic_vector(3 downto 0);
+            output : out std_logic_vector(7 downto 0)
+        );
+    end component;
+
+    component shift_left_2
+        Port (
+            input : in std_logic_vector(3 downto 0);
+            output : out std_logic_vector(7 downto 0)
+        );
+    end component;
+
+    component shift_left_0
+        Port (
+            input : in std_logic_vector(3 downto 0);
+            output : out std_logic_vector(7 downto 0)
+        );
+    end component;
+
+    signal pp1, pp2, pp3, pp4 : std_logic_vector(3 downto 0);
+    signal pp1_shift, pp2_shift, pp3_shift, pp4_shift : std_logic_vector(7 downto 0);
+    signal sum1, sum2 : std_logic_vector(7 downto 0);
 
 begin
-    -- Split 4-bit inputs into 2-bit segments
-    -- AH = a(3-2), AL = a(1-0), BH = b(3-2), BL = b(1-0)
-    -- a high is 2 msb a low is 3 lsb , similar thing for b
-    
-    -- AH * BH (shift left by 4)
-    mul_ah_bh: mul_2x2 port map (
-        a => a(3 downto 2),
-        b => b(3 downto 2),
-        ans => pp1
-    );
-    
-    -- AH * BL (shift left by 2)
-    mul_ah_bl: mul_2x2 port map (
-        a => a(3 downto 2),
-        b => b(1 downto 0),
-        ans => pp2
-    );
-    
-    -- AL * BH (shift left by 2)
-    mul_al_bh: mul_2x2 port map (
-        a => a(1 downto 0),
-        b => b(3 downto 2),
-        ans => pp3
-    );
-    
-    -- AL * BL (no shift)
-    mul_al_bl: mul_2x2 port map (
-        a => a(1 downto 0),
-        b => b(1 downto 0),
-        ans => pp4
-    );
-    
-    -- shift the partial products
+    -- calculate partial products using 2x2 multipliers
+    mul_ah_bh: mul_2x2 port map (a(3 downto 2), b(3 downto 2), pp1);
+    mul_ah_bl: mul_2x2 port map (a(3 downto 2), b(1 downto 0), pp2);
+    mul_al_bh: mul_2x2 port map (a(1 downto 0), b(3 downto 2), pp3);
+    mul_al_bl: mul_2x2 port map (a(1 downto 0), b(1 downto 0), pp4);
 
-    -- shift left by 4  (bits 7 to 4)
-    pp1_shift <= unsigned(pp1) & "0000";    
-    -- shift left by 2  (bits 5 to 2)
-    pp2_shift <= "00" & unsigned(pp2) & "00"; 
-    -- shift left by 2  (bits 5 to 2)
-    pp3_shift <= "00" & unsigned(pp3) & "00"; 
-    -- don't shift      (bits 3 to 0)
-    pp4_shift <= "0000" & unsigned(pp4);      
-    
-    -- sum of all shifted partial products 
-    ans <= std_logic_vector(pp1_shift + pp2_shift + pp3_shift + pp4_shift);
+    -- shift partial products 
+    shift_pp1: shift_left_4 port map (pp1, pp1_shift);
+    shift_pp2: shift_left_2 port map (pp2, pp2_shift);
+    shift_pp3: shift_left_2 port map (pp3, pp3_shift);
+    shift_pp4: shift_left_0 port map (pp4, pp4_shift);
+
+    -- Sum shifted partial products
+    adder1: adder_8bit port map (pp1_shift, pp2_shift, sum1, open);
+    adder2: adder_8bit port map (sum1, pp3_shift, sum2, open);
+    adder3: adder_8bit port map (sum2, pp4_shift, ans, open);
 end structural;
--- src/mul_4x4_with_2x2/mul_4x4_with_2x2_simulate.sh
+++ src/mul_4x4_with_2x2/mul_4x4_with_2x2_simulate.sh
@@ -1,21 +1,53 @@
 #!/bin/bash
 
 DIR="../work"
+NAME="mul_4x4_with_2x2"
 
-if [ ! -d "$DIR" ]; then
-    mkdir "$DIR"
-    echo "The 'work' directory has been created in the parent folder!"
+if [ -d "$DIR" ]; then
+    rm -rf "$DIR"
+    echo "The existing 'work' directory has been removed."
 fi
 
-ghdl -a --workdir=../work ../mul_2x2/mul_2x2.vhdl && \
-echo "analyzing mul_2x2" &&\
+mkdir "$DIR"
+echo "A new 'work' directory has been created in the parent folder!"
 
-ghdl -s -fsynopsys --workdir=../work mul_4x4_with_2x2.vhdl && \
-ghdl -a -fsynopsys --workdir=../work mul_4x4_with_2x2.vhdl && \
-ghdl -s -fsynopsys --workdir=../work mul_4x4_with_2x2_tb.vhdl && \
-ghdl -a -fsynopsys --workdir=../work mul_4x4_with_2x2_tb.vhdl && \
-ghdl -e -fsynopsys --workdir=../work mul_4x4_with_2x2_tb && \
+OPEN_GTKWAVE="no"
+if [ "$1" == "yes" ]; then
+    OPEN_GTKWAVE="yes"
+fi
 
-ghdl -r -fsynopsys --workdir=../work mul_4x4_with_2x2_tb --vcd=mul_4x4.vcd --stop-time=500ns && \
+ghdl -a --workdir="$DIR" "../full_adder/full_adder.vhdl" && \
+echo "Analyzing full_adder..." && \
 
-echo "mul_4x4.vcd is ready"
+ghdl -a --workdir="$DIR" "../mul_2x2/mul_2x2.vhdl" && \
+echo "Analyzing mul_2x2..." && \
+
+ghdl -a --workdir="$DIR" "../adder_8bit/adder_8bit.vhdl" && \
+echo "Analyzing adder_8bit..." && \
+
+ghdl -a --workdir="$DIR" "../shift_left_4/shift_left_4.vhdl" && \
+echo "Analyzing shift_left_4..." && \
+
+ghdl -a --workdir="$DIR" "../shift_left_2/shift_left_2.vhdl" && \
+echo "Analyzing shift_left_2..." && \
+
+ghdl -a --workdir="$DIR" "../shift_left_0/shift_left_0.vhdl" && \
+echo "Analyzing shift_left_0..." && \
+
+
+ghdl -s --workdir="$DIR" "$NAME.vhdl" && \
+ghdl -a --workdir="$DIR" "$NAME.vhdl" && \
+ghdl -s --workdir="$DIR" "${NAME}_tb.vhdl" && \
+ghdl -a --workdir="$DIR" "${NAME}_tb.vhdl" && \
+ghdl -e --workdir="$DIR" "${NAME}_tb" && \
+ghdl -r --workdir="$DIR" "${NAME}_tb" --vcd="${NAME}.vcd" --stop-time=500ns && \
+
+echo "${NAME}.vcd is ready"
+
+# Open GTKWave if requested
+if [ "$OPEN_GTKWAVE" == "yes" ]; then
+    echo "Opening GTKWave..."
+    gtkwave "${NAME}.vcd"
+else
+    echo "GTKWave will not be opened. To open it, run the script with 'yes' as an argument."
+fi
